ab81286a4ac3729ba31d3de72649dd96025a8f06
hyae4924 <hyae4924@gmail.com>
2022-10-29 21:29:09 +0900

    solved 11 of section7

--- inflearn/section7/_10.이분검색.js
+++ inflearn/section7/_10.이분검색.js
@@ -1,5 +1,5 @@
 // 인자로 들어오는 배열의 길이의 범위가 1,000,000까지 가능이니
-// 시간복잡도 o(n)으로 해결해야한다고 판단됨
+// 시간복잡도 o(n)으로 해결해도 된다고 판단됨
 function solution1(target, arr) {
   let count = 0;
   for (let item of arr) {
@@ -8,6 +8,7 @@ function solution1(target, arr) {
   return ++count;
 }
 // 오름차순후 , 이분검색을 활용한 방법
+// 시간복잡도 o(logn)
 function solution2(target, arr) {
   const copy = arr.slice().sort((a, b) => a - b);
   let left = 0;
@@ -15,8 +16,8 @@ function solution2(target, arr) {
   let center;
   for (; target !== copy[center]; ) {
     center = parseInt((left + right) / 2);
-    if (copy[center] > target) right = center;
-    else if (copy[center] < target) left = center;
+    if (copy[center] > target) right = center - 1;
+    else if (copy[center] < target) left = center + 1;
   }
   return center + 1;
 }
@@ -38,5 +39,5 @@ function solution2(target, arr) {
 //   return answer;
 // }
 
-let arr = [23, 87, 65, 12, 57, 32, 99, 81, 16, 21, 20, 19, 1, 2, 3, 4];
+let arr = [23, 87, 65, 12, 57, 32, 99, 81];
 console.log(solution2(32, arr));
--- inflearn/section7/_11.뮤직비디오.js
+++ inflearn/section7/_11.뮤직비디오.js
@@ -1,5 +1,108 @@
-function solution(m, songs) {}
-// Refer
+// 이분검색 이용
+function solution(m, songs) {
+  // DVD의 길이가 될수 있는 범위
+  // 최대의 범위에서 이분검색을 통해 최소의 범위까지 줄여가며
+  //  최적의 답을 찾아보자
+
+  let left = Math.max(...songs);
+  let right = songs.reduce((a, b) => a + b, 0);
+  let center = parseInt((left + right) / 2);
+  let answer = right;
+
+  for (; left <= right; ) {
+    let minute = 0;
+    let count = 1;
+    for (let i = 0; i < songs.length; i++) {
+      if (center >= minute + songs[i]) {
+        minute += songs[i];
+      } else {
+        count++;
+        minute = 0;
+        minute += songs[i];
+      }
+    }
+
+    if (count <= m && answer > center) answer = center;
+    if (count > m) {
+      left = center + 1;
+      center = parseInt((left + right) / 2);
+    } else {
+      right = center - 1;
+      center = parseInt((left + right) / 2);
+    }
+  }
+  return answer;
+}
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(solution(3, arr));
+
+// firstSoultion-----------------------------------
+// const fixArray = array => {
+//   const maxIndex = array.findIndex(
+//     item => item.reduce((a, b) => a + b, 0) === findMax(array)
+//   );
+//   if (maxIndex === 0) {
+//     array[maxIndex + 1].unshift(array[maxIndex].pop());
+//   } else if (maxIndex === array.length - 1) {
+//     array[maxIndex - 1].push(array[maxIndex].shift());
+//   } else {
+//     const frontSum = array[maxIndex - 1].reduce((a, b) => a + b, 0);
+//     const backSum = array[maxIndex + 1].reduce((a, b) => a + b, 0);
+//     let target;
+//     if (frontSum >= backSum) {
+//       target = maxIndex + 1;
+//       array[target].unshift(array[maxIndex].pop());
+//     } else {
+//       target = maxIndex - 1;
+//       array[target].push(array[maxIndex].shift());
+//     }
+//   }
+// };
+// const findMax = array => {
+//   let max = 0;
+//   for (let i = 0; i < array.length; i++) {
+//     const sum = array[i].reduce((a, b) => a + b, 0);
+//     if (sum > max) max = sum;
+//   }
+//   return max;
+// };
+// const findMin = array => {
+//   let min = Number.MAX_SAFE_INTEGER;
+//   for (let i = 0; i < array.length; i++) {
+//     const sum = array[i].reduce((a, b) => a + b, 0);
+//     if (min > sum) min = sum;
+//   }
+//   return min;
+// };
+// function solution(m, songs) {
+//   const chunckSize = parseInt(songs.length / m);
+//   const array = [];
+//   for (let i = 0; i < songs.length; i += chunckSize) {
+//     array.push(songs.slice(i, i + chunckSize));
+//   }
+//   let max = findMax(array);
+//   let min = findMin(array);
+//   let cal = max - min;
+//   let tmpForCal = 0;
+//   let tmpForMax = max;
+//   for (;;) {
+//     fixArray(array);
+//     max = findMax(array);
+//     min = findMin(array);
+//     tmpForCal = max - min;
+//     if (cal < tmpForCal) break;
+//     else {
+//       cal = tmpForCal;
+//       tmpForMax = max;
+//     }
+//   }
+//   return tmpForMax;
+// }
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(solution(3, arr));
+// firstSoultion-----------------------------------
+
+// Refer----------------------------------------
 // function count(songs, capacity) {
 //   let cnt = 1,
 //     sum = 0;
@@ -11,7 +114,6 @@ function solution(m, songs) {}
 //   }
 //   return cnt;
 // }
-
 // function solution(m, songs) {
 //   let answer;
 //   let lt = Math.max(...songs);
@@ -27,6 +129,3 @@ function solution(m, songs) {}
 //   }
 //   return answer;
 // }
-
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-console.log(solution(3, arr));
